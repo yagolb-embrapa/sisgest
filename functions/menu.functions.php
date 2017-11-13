ff1f282216ea2c61e4ef5e0fb9d594730e4da468
yagolb-embrapa <yago.lima@colaborador.embrapa.br>
2017-11-13 11:25:11 -0200

    Primeira atualizacao no menu

--- functions/menu.functions.php
+++ functions/menu.functions.php
@@ -17,108 +17,127 @@ function show_menu ( $id,  $permissoes, $funcao )
 		
 	//Estagiários	
 	echo "
-	<td align='left' width='18%' >
+	<td align='left' width='14%' >
 		<ul id='nav'> 
-			<li ><span>&nbsp;&nbsp;Estagiários</span>		
+			<li ><span>&nbsp;&nbsp;Estágio</span>		
 				<ul>";
-				
 					echo "<li><span onclick=\"top.location.href='../estagiario/estagiario.inclusao.php';\">
-								Incluir Estagiário</span></li>";					
-												
-    if($_SESSION['USERNIVEL'] != 'c'){
-					echo "<li><span onclick=\"top.location.href='../estagiario/estagiario.gerenciamento.php';\">
-								Gerenciar Estagiários</span></li>";
-    }
-											
+								Solicitação</span></li>";																
+    				if($_SESSION['USERNIVEL'] != 'c'){
+						echo "<li><span onclick=\"top.location.href='../estagiario/estagiario.gerenciamento.php';\">
+							Meus estagiários/bolsistas</span></li>";
+    				}						
 				echo "
 				</ul>
 			</li> 
 		</ul>
-	</td>";	
-	
-	
+	</td>";		
     if($_SESSION['USERNIVEL'] != 'c'){
+
 	//Relatórios
-	echo "
-	<td align='left' width='18%' >		
-		<ul id='nav3'> 
-			<li ><span onclick=\"top.location.href='../relatorio/relatorios.lista.php';\">
-					&nbsp;&nbsp;Relatórios</span>
-			</li> 
-		</ul>
-	</td>";
+    if($_SESSION['USERNIVEL'] == 'a') {		
+		echo "
+		<td align='left' width='14%' >		
+			<ul id='nav3'> 
+				<li ><span onclick=\"top.location.href='../relatorio/relatorios.lista.php';\">
+						&nbsp;&nbsp;Relatórios</span>
+				</li> 
+			</ul>
+		</td>";
+	}
 
 	//Termos
-	echo "
-	<td align='left' width='18%' >		
-		<ul id='nav3'> 
-			<li ><span onclick=\"top.location.href='../termo/termo.lista.php';\">
-					&nbsp;&nbsp;Termos</span>
-			</li> 
-		</ul>
-	</td>";	
-        
-	//Configurações		
-	echo "
-	<td align='left' width='23%' >		
-		<ul id='nav2'> 
-			<li ><span>&nbsp;&nbsp;Configurações</span>		
-					<ul>";
-					
-                        echo "<li><span onclick=\"top.location.href='../emails/emails.gerenciamento.php';\">
-                            Gerenciar Emails</span></li>";
-                                                
+	if($_SESSION['USERNIVEL'] == 'a'){		
+		echo "
+		<td align='left' width='13%' >		
+			<ul id='nav3'> 
+				<li ><span onclick=\"top.location.href='../termo/termo.lista.php';\">
+						&nbsp;&nbsp;Termos</span>
+				</li> 
+			</ul>
+		</td>";	
+    }    
+
+    /* Decidindo quem podera acessar
+    //Acompanhamento
+    if($_SESSION['USERNIVEL'] == 'a') {		
+		echo "
+		<td align='left' width='20%' >		
+			<ul id='nav3'> 
+				<li ><span onclick=\"top.location.href='../acompanhamento/acompanhamento.php';\">
+						&nbsp;&nbsp;Acompanhamento</span>
+				</li> 
+			</ul>
+		</td>";
+	}*/
+
+	//Configurações	
+	if($_SESSION['USERNIVEL'] == 'a'){		
+		echo "
+		<td align='left' width='20%' >		
+			<ul id='nav2'> 
+				<li ><span>&nbsp;&nbsp;Configurações</span>		
+					<ul>";		
+	                	echo "<li><span onclick=\"top.location.href='../emails/emails.gerenciamento.php';\">
+    	                	Gerenciar Emails</span></li>";                
 						echo "<li><span onclick=\"top.location.href='../bancos/bancos.gerenciamento.php';\">
-                            Gerenciar Bancos</span></li>";
-					    
+            	            Gerenciar Bancos</span></li>";
 						echo "<li><span onclick=\"top.location.href='../instituicao/instituicao.gerenciamento.php';\">
-                            Gerenciar Instituições</span></li>";
-
+                    	    Gerenciar Instituições</span></li>";
 						echo "<li><span onclick=\"top.location.href='../bolsista/bolsista.gerenciamento.php';\">
-                            Gerenciar Modalidades de Bolsa</span></li>";
-
+                          	Gerenciar Modalidades de Bolsa</span></li>";
 						echo "<li><span onclick=\"top.location.href='../origens_recursos/origem.gerenciamento.php';\">
-                            Gerenciar Origens de Recursos</span></li>";
-
+                           	Gerenciar Origens de Recursos</span></li>";
 						echo "<li><span onclick=\"top.location.href='../supervisor/supervisor.gerenciamento.php';\">
-                            Gerenciar Supervisores</span></li>";
-													
-						if($_SESSION['USERNIVEL'] == 'a'){		
-							echo "<li><span onclick=\"top.location.href='../usuario/usuario.gerenciamento.php';\">
-								Gerenciar Usuários</span></li>";
-						}
+                           	Gerenciar Supervisores</span></li>";					
+						echo "<li><span onclick=\"top.location.href='../usuario/usuario.gerenciamento.php';\">
+							Gerenciar Usuários</span></li>";
+						echo"
+					</ul>
+				</li> 
+			</ul>
+		</td>";		
+	}
 
+	//Contas		
+	if($_SESSION['USERNIVEL'] == 'a'){	
+		echo "
+		<td align='left' width='13%' >		
+			<ul id='nav2'> 
+				<li ><span>&nbsp;&nbsp;Contas</span>		
+					<ul>";					
+						echo "<li><span onclick=\"top.location.href='../conta/conta.abertura.php';\">
+							Abertura de Contas</span></li>";					
+						echo "<li><span onclick=\"top.location.href='../conta/conta.finalizacao.php';\">
+							Finalização de Contas</span></li>";	
+					echo"
+					</ul>
+				</li> 
+			</ul>
+		</td>";
+	}
+
+	//Ajuda	
+	echo "
+	<td align='left' width='13%' >		
+		<ul id='nav2'> 
+			<li ><span>&nbsp;&nbsp;Ajuda</span>		
+				<ul>";										
+					echo "<li><span onclick=\"top.location.href='../ajuda/ajuda.tutorial.pdf';\">
+						Tutorial</span></li>";	
 				echo"
 				</ul>
 			</li> 
 		</ul>
-	</td>";		
-	
-	//Contas		
-	echo "
-	<td align='left' width='23%' >		
-		<ul id='nav2'> 
-			<li ><span>&nbsp;&nbsp;Contas</span>		
-					<ul>";					
-						echo "<li><span onclick=\"top.location.href='../conta/conta.abertura.php';\">
-								Abertura de Contas</span></li>";					
-						echo "<li><span onclick=\"top.location.href='../conta/conta.finalizacao.php';\">
-								Finalização de Contas</span></li>";	
-	echo"
-				</ul>
-			</li> 
-		</ul>
 	</td>";
-		
     }
+
 	//Sair	
-	echo "<td align='center' width='11%'><ul><li>
+	echo "<td align='center' width='13%'><ul><li>
 		<span onclick=\"top.location.href='../logout.php';\">Sair</span></td>
 	</li></ul>
 	</tr>
 	</table></div><br>";
-	
-
 }
 
 
@@ -131,109 +150,126 @@ function show_menu_root ( $id,  $permissoes, $funcao )
 		
 	//Estagiários	
 	echo "
-	<td align='left' width='18%' >
+	<td align='left' width='14%' >
 		<ul id='nav'> 
-			<li ><span>&nbsp;&nbsp;Estagiários</span>		
+			<li ><span>&nbsp;&nbsp;Estágio</span>		
 				<ul>";
-				
 					echo "<li><span onclick=\"top.location.href='estagiario/estagiario.inclusao.php';\">
-								Incluir Estagiário</span></li>";					
-												
-    if($_SESSION['USERNIVEL'] != 'c'){
-					echo "<li><span onclick=\"top.location.href='estagiario/estagiario.gerenciamento.php';\">
-								Gerenciar Estagiários</span></li>";
-    }
-											
+						Solicitação</span></li>";															
+			    	if($_SESSION['USERNIVEL'] != 'c'){
+						echo "<li><span onclick=\"top.location.href='estagiario/estagiario.gerenciamento.php';\">
+							Meus estagiários/bolsistas</span></li>";
+    				}						
 				echo "
 				</ul>
 			</li> 
 		</ul>
 	</td>";	
-		        
     if($_SESSION['USERNIVEL'] != 'c'){
+
 	//Relatórios
-	echo "
-	<td align='left' width='18%' >		
-		<ul id='nav3'> 
-			<li ><span onclick=\"top.location.href='relatorio/relatorios.lista.php';\">
+    if($_SESSION['USERNIVEL'] == 'a'){		
+		echo "
+		<td align='left' width='14%' >		
+			<ul id='nav3'> 
+				<li ><span onclick=\"top.location.href='relatorio/relatorios.lista.php';\">
 					&nbsp;&nbsp;Relatórios</span>
-			</li>  
-		</ul>
-	</td>";	
-	
+				</li>  
+			</ul>
+		</td>";	
+	}
+
 	//Termos
-	echo "
-	<td align='left' width='18%' >		
-		<ul id='nav3'> 
-			<li ><span onclick=\"top.location.href='termo/termo.lista.php';\">
+	if($_SESSION['USERNIVEL'] == 'a'){		
+		echo "
+		<td align='left' width='13%' >		
+			<ul id='nav3'> 
+				<li ><span onclick=\"top.location.href='termo/termo.lista.php';\">
 					&nbsp;&nbsp;Termos</span>
-			</li> 
-		</ul>
-	</td>";	
-	
-	//Configurações		
-	echo "
-	<td align='left' width='23%' >		
-		<ul id='nav2'> 
-			<li ><span>&nbsp;&nbsp;Configurações</span>		
-					<ul>";						
+				</li> 
+			</ul>
+		</td>";	
+	}
+
+	/* Decidindo quem tera acesso
+	//Acompanhamento
+    if($_SESSION['USERNIVEL'] == 'u') {		
+		echo "
+		<td align='left' width='20%' >		
+			<ul id='nav3'> 
+				<li ><span onclick=\"top.location.href='acompanhamento/acompanhamento.php';\">
+						&nbsp;&nbsp;Acompanhamento</span>
+				</li> 
+			</ul>
+		</td>";
+	}*/
+
+	//Configurações	
+	if($_SESSION['USERNIVEL'] == 'a'){		
+		echo "
+		<td align='left' width='20%' >		
+			<ul id='nav2'> 
+				<li ><span>&nbsp;&nbsp;Configurações</span>		
+					<ul>";								
 						echo "<li><span onclick=\"top.location.href='emails/emails.gerenciamento.php';\">
                             Gerenciar Emails</span></li>";
-						
 						echo "<li><span onclick=\"top.location.href='bancos/bancos.gerenciamento.php';\">
                             Gerenciar Bancos</span></li>";
-					    
 						echo "<li><span onclick=\"top.location.href='instituicao/instituicao.gerenciamento.php';\">
                             Gerenciar Instituições</span></li>";
-													
 					    echo "<li><span onclick=\"top.location.href='bolsista/bolsista.gerenciamento.php';\">
 						    Gerenciar Modalidades de Bolsa</span></li>";
-
 						echo "<li><span onclick=\"top.location.href='origens_recursos/origem.gerenciamento.php';\">
                             Gerenciar Origens de Recursos</span></li>";
-						
 						echo "<li><span onclick=\"top.location.href='supervisor/supervisor.gerenciamento.php';\">
                             Gerenciar Supervisores</span></li>";
+						echo "<li><span onclick=\"top.location.href='usuario/usuario.gerenciamento.php';\">
+							Gerenciar Usuários</span></li>";
+					echo"
+					</ul>
+				</li> 
+			</ul>
+		</td>";			
+	}
 
-						if($_SESSION['USERNIVEL'] == 'a'){		
-							echo "<li><span onclick=\"top.location.href='usuario/usuario.gerenciamento.php';\">
-								Gerenciar Usuários</span></li>";
-						}			
+	//Contas	
+	if($_SESSION['USERNIVEL'] == 'a'){		
+		echo "
+		<td align='left' width='13%' >		
+			<ul id='nav2'> 
+				<li ><span>&nbsp;&nbsp;Contas</span>		
+					<ul>";								
+						echo "<li><span onclick=\"top.location.href='conta/conta.abertura.php';\">
+							Abertura de Contas</span></li>";					
+						echo "<li><span onclick=\"top.location.href='conta/conta.finalizacao.php';\">
+							Finalização de Contas</span></li>";	
+					echo"
+					</ul>
+				</li> 
+			</ul>
+		</td>";
+	}
 
+	//Ajuda	
+	echo "
+	<td align='left' width='13%' >		
+		<ul id='nav2'> 
+			<li ><span>&nbsp;&nbsp;Ajuda</span>		
+				<ul>";										
+				echo "<li><span onclick=\"top.location.href='ajuda/ajuda.tutorial.pdf';\">
+					Tutorial</span></li>";	
 				echo"
 				</ul>
 			</li> 
 		</ul>
-	</td>";			
-	
-	//Contas		
-	echo "
-	<td align='left' width='23%' >		
-		<ul id='nav2'> 
-			<li ><span>&nbsp;&nbsp;Contas</span>		
-					<ul>";				
-									
-						echo "<li><span onclick=\"top.location.href='conta/conta.abertura.php';\">
-								Abertura de Contas</span></li>";					
-						echo "<li><span onclick=\"top.location.href='conta/conta.finalizacao.php';\">
-								Finalização de Contas</span></li>";	
-	echo"
-				</ul>
-			</li> 
-		</ul>
 	</td>";
-		
     }
+
 	//Sair	
-	echo "<td align='center' width='11%'><ul><li>
+	echo "<td align='center' width='13%'><ul><li>
 		<span onclick=\"top.location.href='logout.php';\">Sair</span></td>
 		</li></ul>
 		</tr>
 		</table></div><br>";
-	
-
-}
-
-
- 
+} 
 ?>
